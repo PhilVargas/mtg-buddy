fb771feb46a58bfa69362f722f25b50119005673
Phil Vargas <philipavargas@gmail.com>
2015-10-16 18:42:57 -0700

    feat(searches#show) manually route the search form

--- client/templates/searches/helpers.js
+++ client/templates/searches/helpers.js
@@ -1,1 +1,7 @@
+Template.SearchIndex.events({
+  'submit #search-form': function(e){
+    e.preventDefault();
+    Router.go('search.show', { query: $(e.target).serialize() }, { query: $(e.target).serialize() });
+  }
+})
 
