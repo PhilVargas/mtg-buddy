69a55b2f9d8919fe285b4208aced6b0dd1ade64c
Phil Vargas <philipavargas@gmail.com>
2015-10-19 13:12:26 -0700

    feat(seeds.js) add seeds file and seed data
     - add the sets and blocks collection
    
     - add raw json data for innistrad and rtr block
    
     - add seedjs file which can be run from meteor toys
    
    search-form

---
+++ server/seeds.js
@@ -0,0 +1,72 @@
+Meteor.methods({
+  seed(){
+    let isd, dka, avr, rtr, gtc, dgm, seeds;
+
+    isd = Assets.getText('ISD.json');
+    dka = Assets.getText('DKA.json');
+    avr = Assets.getText('AVR.json');
+    gtc = Assets.getText('GTC.json');
+    dgm = Assets.getText('DGM.json');
+    rtr = Assets.getText('RTR.json');
+
+    seeds = [isd, dka, avr, rtr, gtc, dgm];
+
+    seeds.forEach(function(seed){
+      let block, set;
+
+      seed = JSON.parse(seed);
+
+      if (!Blocks.findOne({ name: seed.block })) {
+        Blocks.insert({ name: seed.block, createdAt: new Date(), setIds: [], cardIds: [] });
+      }
+
+      block = Blocks.findOne({ name: seed.block });
+
+      if (!Sets.findOne({ name: seed.name })) {
+        let setId;
+
+        setId = Sets.insert({
+          name: seed.name,
+          setCode: seed.code,
+          blockId: block._id,
+          createdAt: new Date(),
+          cardIds: [],
+          releaseDate: new Date(seed.releaseDate)
+        });
+
+        Blocks.update({ _id: block._id }, { $push: { setIds: setId } });
+      }
+
+      set = Sets.findOne({ name: seed.name });
+
+      seed.cards.forEach(function(card){
+        if (!Cards.findOne({ multiverseid: card.multiverseid })) {
+          let attribs, cardId;
+
+          attribs = {
+            multiverseid: card.multiverseid,
+            name: card.name,
+            colors: card.colors || ['colorless'],
+            subtypes: card.subtypes || [],
+            supertypes: card.supertypes || [],
+            power: card.power || 0,
+            toughness: card.toughness || 0,
+            cmc: card.cmc || 0,
+            rarity: card.rarity,
+            manaCost: card.manaCost || '{0}',
+            oracle: card.text,
+            flavor: card.flavor,
+            setId: set._id,
+            blockId: block._id,
+            imageUrl: `http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${card.multiverseid}&type=card`,
+            createdAt: new Date()
+          };
+
+          cardId = Cards.insert(attribs);
+          Blocks.update({ _id: block._id }, { $push: { cardIds: cardId } });
+          Sets.update({ _id: set._id }, { $push: { cardIds: cardId } });
+        }
+      });
+    });
+  }
+});
