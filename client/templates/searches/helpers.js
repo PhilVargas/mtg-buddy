2672e4af89d9462073064598c582f1c1781fd099
Phil Vargas <philipavargas@gmail.com>
2015-10-20 09:21:03 -0700

    feat(search form) search by colors in search form
     - extract the search form out into its own partial
    
     - create helper method for generating checkbox html
    
     - temporarily display colors on card show page
    
    search-form

--- client/templates/searches/helpers.js
+++ client/templates/searches/helpers.js
@@ -1,3 +1,5 @@
+let checkboxColors;
+
 Template.SearchIndex.events({
   'submit #search'(e){
     e.preventDefault();
@@ -5,3 +7,59 @@ Template.SearchIndex.events({
   }
 });
 
+Template.SearchForm.helpers({
+  colors(){
+    return [
+      { colorGroup: checkboxColors.mono },
+      { colorGroup: checkboxColors.dual },
+      { colorGroup: checkboxColors.tri }
+    ];
+  },
+  monoColors(){
+    return checkboxColors.mono;
+  },
+
+  dualColors(){
+    return checkboxColors.dual;
+  },
+
+  triColors(){
+    return checkboxColors.tri;
+  }
+});
+
+checkboxColors = {
+  mono: [
+    { value: 'White', icon: ['w'], label: 'White' },
+    { value: 'Blue', icon: ['u'], label: 'Blue' },
+    { value: 'Black', icon: ['b'], label: 'Black' },
+    { value: 'Red', icon: ['r'], label: 'Red' },
+    { value: 'Green', icon: ['g'], label: 'Green' },
+    { value: 'Colorless', icon: ['x'], label: 'Colorless' }
+  ],
+  dual: [
+    { value: 'White-Blue', icon: ['w', 'u'], label: 'Azorius' },
+    { value: 'White-Black', icon: ['w', 'b'], label: 'Orzhov' },
+    { value: 'White-Red', icon: ['w', 'r'], label: 'Boros' },
+    { value: 'White-Green', icon: ['w', 'g'], label: 'Selesnya' },
+    { value: 'Blue-Black', icon: ['u', 'b'], label: 'Dimir' },
+    { value: 'Blue-Red', icon: ['u', 'r'], label: 'Izzet' },
+    { value: 'Blue-Green', icon: ['u', 'g'], label: 'Simic' },
+    { value: 'Black-Red', icon: ['b', 'r'], label: 'Rakdos' },
+    { value: 'Black-Green', icon: ['b', 'g'], label: 'Golgari' },
+    { value: 'Red-Green', icon: ['r', 'g'], label: 'Gruul' }
+  ],
+  tri: [
+    { value: 'White-Black-Green', icon: ['w', 'b', 'g'], label: 'Abzan' },
+    { value: 'White-Green-Blue', icon: ['w', 'g', 'u'], label: 'Bant' },
+    { value: 'Blue-Red-White', icon: ['u', 'r', 'w'], label: 'Jeskai' },
+    { value: 'Blue-White-Black', icon: ['u', 'w', 'b'], label: 'Esper' },
+    { value: 'Black-Green-Blue', icon: ['b', 'g', 'u'], label: 'Sultai' },
+    { value: 'Black-Blue-Red', icon: ['b', 'u', 'r'], label: 'Grixis' },
+    { value: 'Red-White-Black', icon: ['r', 'w', 'b'], label: 'Mardu' },
+    { value: 'Red-Black-Green', icon: ['r', 'b', 'g'], label: 'Jund' },
+    { value: 'Green-Blue-Red', icon: ['g', 'u', 'r'], label: 'Temur' },
+    { value: 'Green-Red-White', icon: ['g', 'r', 'w'], label: 'Naya' }
+  ],
+  all: []
+};
